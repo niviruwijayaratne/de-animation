2ae8c7b70831d2647184512dbef71cc6f672d39d
niviruwijayaratne <nnw2@illinois.edu>
2021-04-21 04:11:17 -0500

    updated save intermediate functionality

--- klt.py
+++ klt.py
@@ -9,7 +9,7 @@ class Tracker():
         self.inputPath = config['ioParams']['inputPath']
         self.saveIntermediate = config['ioParams']['intermediateResults']
         self.outdir = os.path.join(os.getcwd(), 'results') if self.saveIntermediate else None
-        self.outputPath = os.path.join(self.outdir, config['ioParams']['outputPath'])
+        self.outputPath = os.path.join(self.outdir, config['ioParams']['outputPath']) if self.outdir else None
         self.featureParams = config['featureParams']
         self.trackingParams = config['trackingParams']
 
@@ -28,7 +28,10 @@ class Tracker():
     def track_features(self):
         vid = cv2.VideoCapture(self.inputPath)
         ret, start_frame = vid.read()
-        writer = cv2.VideoWriter(self.outputPath ,cv2.VideoWriter_fourcc(*'mp4v'), 20.0, (start_frame.shape[1], start_frame.shape[0]))
+        if self.saveIntermediate:
+            writer = cv2.VideoWriter(self.outputPath ,cv2.VideoWriter_fourcc(*'mp4v'), 20.0, (start_frame.shape[1], start_frame.shape[0]))
+        else:
+            writer = None
         start_gray = cv2.cvtColor(start_frame, cv2.COLOR_BGR2GRAY)
         corners = cv2.goodFeaturesToTrack(start_gray, **self.featureParams)
         if self.saveIntermediate:
@@ -36,32 +39,34 @@ class Tracker():
 
         color = np.random.randint(0, 255, (self.featureParams['maxCorners'], 3))
         mask = np.zeros_like(start_frame)
-        
+        old_corners = corners
         while True:
             ret, frame = vid.read() 
             if frame is None:
                 break
 
             frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            p1, st, err = cv2.calcOpticalFlowPyrLK(start_gray, frame_gray, corners, None, **self.trackingParams)
-            good_new = p1[st == 1]
-            good_old = corners[st==1]
-            for i, (new, old) in enumerate(zip(good_new, good_old)):
+            new_corners, exists, error = cv2.calcOpticalFlowPyrLK(start_gray, frame_gray, old_corners, None, **self.trackingParams)
+            good_new_corners = new_corners[exists == 1]
+            good_old_corners = old_corners[exists==1]
+            for i, (new, old) in enumerate(zip(good_new_corners, good_old_corners)):
                 a, b = new.ravel()
                 c, d = old.ravel()
                 mask = cv2.line(mask, (a,b), (c, d), color[i].tolist(), 5)
                 frame = cv2.circle(frame, (a,b), 2, color[i].tolist(), 5)
             img = cv2.add(frame, mask)
-            writer.write(img)
+            if writer:
+                writer.write(img)
             cv2.imshow('frame',img)
             k = cv2.waitKey(30) & 0xff
             if k == 27:
                 break
 
             start_gray = frame_gray.copy()
-            corners = good_new.reshape(-1, 1, 2)
+            old_corners = good_new_corners.reshape(-1, 1, 2)
         vid.release()
-        writer.release()
+        if writer:
+            writer.release()
 
     def construct_table(self):
         pass
