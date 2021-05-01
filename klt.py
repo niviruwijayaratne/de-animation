klt bugs fixed, initial lsq set up, need to integrate weighting functions

klt.py
@@ -3,6 +3,7 @@ import numpy as np
 import argparse
 import yaml
 import os
+import sys
 
 class Tracker():
     def __init__(self, config):
@@ -14,10 +15,18 @@ class Tracker():
         self.trackingParams = config['trackingParams']
         self.meshSize = config['meshSize']
         self.reference_frame = None
+
+        self.feature_table = None
+        self.vertices = None
+        self.quads = None
+        self.quad_dict = {}
+        self.solved_quad_indices = None
+        self.weights_and_verts = None
+        self.solved_quad_indices = []
+
         if self.outdir and not os.path.exists(self.outdir):
             os.mkdir(self.outdir)
         
-
     def draw_corners(self, img: np.ndarray, corners: np.ndarray):
         corners = np.int0(corners)
         for i in corners:
@@ -47,7 +56,6 @@ class Tracker():
         old_corners = corners
         frame_count = 0
         feature_tracks = {}
-        
         while True:
             ret, frame = vid.read() 
             if frame is None:
@@ -55,7 +63,7 @@ class Tracker():
             feature_tracks[frame_count] = np.zeros((corners.shape[0], 2))
             frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             new_corners, exists, error = cv2.calcOpticalFlowPyrLK(start_gray, frame_gray, old_corners, None, **self.trackingParams)
-            feature_tracks[frame_count] = old_corners
+            feature_tracks[frame_count] = np.flip(old_corners, axis=2)
             
             good_new_corners = new_corners
             good_new_corners[~exists.all(axis=1)] = [0, 0]
@@ -76,25 +84,22 @@ class Tracker():
             #     break
             start_gray = frame_gray.copy()
             old_corners = good_new_corners.reshape(-1, 1, 2)
-            if frame_count >= 50:
+            if frame_count >= 5:
                 break
             frame_count += 1
             
         if writer:
             writer.release()
 
-        return self.construct_table(feature_tracks)
+        self.construct_table(feature_tracks)
 
-    def construct_table(self, feature_tracks: dict) -> np.ndarray:
-        table = np.zeros((self.featureParams['maxCorners'], max(feature_tracks.keys()) + 1, 2))
+    def construct_table(self, feature_tracks) -> np.ndarray:
+        self.feature_table = np.zeros((self.featureParams['maxCorners'], max(feature_tracks.keys()) + 1, 2))
         count = 0
         for t in feature_tracks.keys():
             for s in range(self.featureParams['maxCorners']):
-                table[s, t] = np.squeeze(feature_tracks[t][s])[::-1]
-        return table
-
+                self.feature_table[s, t] = np.squeeze(feature_tracks[t][s])
     def get_mesh(self):
-        # print("Getting Mesh")
         '''
         Returns: 64*32 x 4 x 2 ndarray where each row indexes a mesh quad (raster order)
         and gives the 4x2 vector of the vertices that define that quad; order = 
@@ -108,100 +113,88 @@ class Tracker():
         assert(len(x_coords) == self.meshSize[1] + 1)
         assert(len(y_coords) == self.meshSize[0] + 1)
         x_grid, y_grid = np.meshgrid(x_coords, y_coords)
-        vertices = []
-        for y in y_grid:
-            vertices.append(np.array(list(zip(y, x_grid[0]))))
-        vertices = np.array(vertices)
+        
+        self.vertices = np.zeros((65, 33, 2))
+        for i, y in enumerate(y_grid):
+            self.vertices[i, :, :] = np.array(list(zip(y, x_grid[0])))
 
-        x_steps1 = np.arange(0, vertices.shape[1] + 1, 2)
-        x_steps2 = np.arange(1, vertices.shape[1] + 2, 2)
-        y_steps1 = np.arange(0, vertices.shape[0] + 1, 2)
-        y_steps2 = np.arange(1, vertices.shape[0] + 2, 2)
+        x_steps1 = np.arange(0, self.vertices.shape[1] + 1, 2)
+        x_steps2 = np.arange(1, self.vertices.shape[1] + 2, 2)
+        y_steps1 = np.arange(0, self.vertices.shape[0] + 1, 2)
+        y_steps2 = np.arange(1, self.vertices.shape[0] + 2, 2)
 
-        quads = []
+        self.quads = np.zeros((64*32, 4,  2))
+        count = 0
         for j in range(len(y_steps1) - 1):
             for i in range(len(x_steps1) - 1):
-                pairs = np.squeeze(vertices[y_steps1[j]:y_steps1[j + 1], x_steps1[i]:x_steps1[i+1]])
-                quads.append(np.array([pairs[0, 0], pairs[0, 1], pairs[1, 0], pairs[1, 1]]))
-                pairs = np.squeeze(vertices[y_steps1[j]:y_steps1[j + 1], x_steps2[i]:x_steps2[i+1]])
-                quads.append(np.array([pairs[0, 0], pairs[0, 1], pairs[1, 0], pairs[1, 1]]))
+                pairs = np.squeeze(self.vertices[y_steps1[j]:y_steps1[j + 1], x_steps1[i]:x_steps1[i+1]])
+                self.quads[count] = np.array([pairs[0, 0], pairs[0, 1], pairs[1, 0], pairs[1, 1]])
+                count += 1
+                pairs = np.squeeze(self.vertices[y_steps1[j]:y_steps1[j + 1], x_steps2[i]:x_steps2[i+1]])
+                self.quads[count] = np.array([pairs[0, 0], pairs[0, 1], pairs[1, 0], pairs[1, 1]])
+                count += 1
             
             for i in range(len(x_steps1) - 1):
-                pairs = np.squeeze(vertices[y_steps2[j]:y_steps2[j + 1], x_steps1[i]:x_steps1[i+1]])
-                quads.append(np.array([pairs[0, 0], pairs[0, 1], pairs[1, 0], pairs[1, 1]]))
-                pairs = np.squeeze(vertices[y_steps2[j]:y_steps2[j + 1], x_steps2[i]:x_steps2[i+1]])
-                quads.append(np.array([pairs[0, 0], pairs[0, 1], pairs[1, 0], pairs[1, 1]]))
+                pairs = np.squeeze(self.vertices[y_steps2[j]:y_steps2[j + 1], x_steps1[i]:x_steps1[i+1]])
+                self.quads[count] = np.array([pairs[0, 0], pairs[0, 1], pairs[1, 0], pairs[1, 1]])
+                count += 1
+                pairs = np.squeeze(self.vertices[y_steps2[j]:y_steps2[j + 1], x_steps2[i]:x_steps2[i+1]])
+                self.quads[count] = np.array([pairs[0, 0], pairs[0, 1], pairs[1, 0], pairs[1, 1]])
+                count += 1
 
-        quads = np.array(quads).astype(np.float32) #64*32 x 4 x 2 matrix
-        quad_dict = self.construct_quad_dict(vertices, quads)
-        return vertices, quads, quad_dict
-        
-    def construct_quad_dict(self, vertices, quads):
-
+    def construct_quad_dict(self):
         '''
         Returns dict()
-        key: byte representation of vertext coordinates
-        value: 1x4 ndarray where each each entry in the array corresponds to a quad that includes this point 
-        and each index in the array indicates the position in the corresponding quad that the vertex is in, ordered from [tl, tr, bl, br]
-        ie. [1, 2, 3, 4] indicates that this point is in quad 1 in the top left, quad 2 in the top right, quad 3 in the bottom left, and 
-        quad 4 in the bottom right
-
-        access key coordinate, np.frombuffer(key, dtype=np.float64)
+        key: tuple(y, x pair)
+        value: int that gives index in quads to the quad that key is in the top left corner
         '''
-        quad_dict = {}
-        for row in vertices:
+        for row in self.vertices:
             for pair in row:
-                quad_dict[pair.tobytes()] = np.zeros(4)
-                in_quad = 0
-                for i, quad in enumerate(quads):
-                    if (pair == quad).all(axis=1).any():
-                        quad_dict[pair.tobytes()][np.where((pair == quad).all(axis=1))] = i
-        return quad_dict
+                for i, quad in enumerate(self.quads):
+                    eq = (pair == quad).all(axis=1)
+                    if eq.any():
+                        if np.where(eq)[0] != 0:
+                            continue
+                        else:
+                            self.quad_dict[tuple(pair)] = i
         
-    def search_quads(self, feature_table, quad_dict): 
-        # print("Searching Quads")
+    def search_quads(self): 
         '''
         Returns # of frames x s ndarray where each row gives the list of quads that include all feature point in that frame
         and each index in that row corresponds to the quad that contains the feature point with the same index in the tracktable
         '''
-        solved_quad_indices = []
-        features = feature_table.transpose(1, 0, 2) #frame_number x feature number (y, x) coords
-        steps = np.array([1280/64, 720/32])
+        features = self.feature_table.transpose(1, 0, 2) #frame_number x feature number (y, x) coords
+        steps = np.array([self.reference_frame.shape[0]/64, self.reference_frame.shape[1]/32])
         closest = np.floor(features/steps)
         top_left = np.multiply(closest, steps)
-        solved_quad_indices = np.zeros_like(features[:, :, 0])
+        self.solved_quad_indices = np.zeros_like(features[:, :, 0])
         for i, row in enumerate(top_left):
             for j, point in enumerate(row):
-                if point.tobytes() in quad_dict.keys():
-                    solved_quad_indices[i, j] = quad_dict[point.tobytes()][0]
-        solved_quad_indices = solved_quad_indices.astype(np.int64)
-        return solved_quad_indices
+                if tuple(point) in self.quad_dict.keys():
+                    self.solved_quad_indices[i, j] = self.quad_dict[tuple(point)]
+        self.solved_quad_indices = self.solved_quad_indices.astype(np.int64)
 
-    def get_weights(self, feature_table, quads, quad_indices, vertices):
-        # print("Getting Weights")
+    def get_weights(self):
         '''
         Returns num_frames x num_features x 2x4 where each index in a row corresponds to a 2x4 array where the first row contains the weights 
         and the second row contains the point indices in an array that is a 2-column matrix of self.xy_pairs repeated t times in [tl, tr, bl, br] order
         '''
-        features = feature_table.transpose(1, 0, 2)
-        vertices = vertices.reshape(-1, 2)
+        features = self.feature_table.transpose(1, 0, 2)
+        vertices = self.vertices.reshape(-1, 2)
         frame_adder = 0
-        weights_and_verts = np.zeros((features.shape[0], features.shape[1], 2, 4))
-        for i, row in enumerate(quad_indices):
+        self.weights_and_verts = np.zeros((features.shape[0], features.shape[1], 2, 4))
+        for i, row in enumerate(self.solved_quad_indices):
             frame_adder = i*len(vertices)
             for j, quad_idx in enumerate(row):
-                quad_vertices = quads[quad_idx]
+                quad_vertices = self.quads[quad_idx]
                 feature_point = features[i, j]
                 weights = self.get_quad_weights(feature_point, quad_vertices)
                 solved_vertices = []
                 for vertex in quad_vertices:
                     solved_vertices.append(int(np.where((vertices == vertex).all(axis=1))[0]) + frame_adder)
-                weights_and_verts[i, j, :, :] = np.array([weights, solved_vertices])
-
-        return weights_and_verts
+                self.weights_and_verts[i, j, :, :] = np.array([weights, solved_vertices])
 
     def get_quad_weights(self, feature_point, quad_vertices):
-        # print("Getting Quad Weights")
         '''
         Inputs:
         - features: np.ndarray of shape (1x2) (x, y) coordinates of a feature point
@@ -224,12 +217,12 @@ class Tracker():
         return [tl_weight, tr_weight, bl_weight, br_weight]
     
     def run(self):
-        feature_table = self.track_features()
-        vertices, quads, quad_dict = self.get_mesh()
-        quad_indices = self.search_quads(feature_table, quad_dict)
-        weights_and_verts = self.get_weights(feature_table, quads, quad_indices, vertices)
+        self.track_features()
+        self.get_mesh()
+        self.construct_quad_dict()
+        self.search_quads()
+        self.get_weights()
 
-        return feature_table, vertices, quads, quad_dict, quad_indices, weights_and_verts
 
 def main():
     parser = argparse.ArgumentParser()
@@ -242,7 +235,7 @@ def main():
         config = yaml.load(f, Loader=yaml.FullLoader)
 
     tracker = Tracker(config)
-    feature_table, vertices, quads, quad_dict, quad_indices, weights_and_verts = tracker.run()
+    tracker.run()
     
 if __name__ == '__main__':
     main()

least-squares-solver.py
@@ -6,111 +6,140 @@ from math import sqrt
 from klt import Tracker
 import yaml
 import argparse
+import sys
 
 class LeastSquaresSolver:
-    def __init__(self, feature_table, vertices, weights_and_verts, quads):
-        self.ka, self.vertices, self.weights_and_verts, self.non_solved_verts, self.processed_quads = self.pre_process(feature_table, vertices, weights_and_verts, quads)
-        num_t = feature_table.shape[1]
-        num_s = feature_table.shape[0]
-        ka = self.ka
-        vertices = self.vertices 
-        weights_and_verts = self.weights_and_verts
-        non_solved_verts = self.non_solved_verts
-        processed_quads = self.processed_quads
-        self.energy_function_lsq(num_s, num_t, ka, vertices, weights_and_verts, non_solved_verts, processed_quads)
+    def __init__(self, tracker):
+        self.tracker = tracker
+        self.ka = None
+        self.vertices = None
+        self.weights_and_verts = None
+        self.non_solved_verts = None 
+        self.processed_quads = None
+        self.pre_process()
+        v_prime = self.energy_function_lsq()
+        self.draw_rectangles(v_prime)
 
-    def pre_process(self, feature_table, vertices, weights_and_verts, quads):
-        num_s = feature_table.shape[0]
-        num_t = feature_table.shape[1]
-        K_a = feature_table.transpose(1, 0, 2)[0].reshape(-1, 1)
-        K_a = np.vstack([K_a for i in range(num_t)])
-        assert(K_a.shape == (num_s*num_t*2, 1))
+    def pre_process(self):
+        num_s = self.tracker.feature_table.shape[0]
+        num_t = self.tracker.feature_table.shape[1]
+        self.ka = self.tracker.feature_table.transpose(1, 0, 2)[0].reshape(-1, 1)
+        self.ka = np.vstack([self.ka for i in range(num_t)])
 
-        processed_quads = np.zeros((64*32, 4))
-        vertices = vertices.reshape(-1, 2)
-        for i, quad in enumerate(quads):
+        assert(self.ka.shape == (num_s*num_t*2, 1))
+
+        self.processed_quads = np.zeros((64*32, 4))
+        self.tracker.vertices = self.tracker.vertices.reshape(-1, 2)
+        for i, quad in enumerate(self.tracker.quads):
             quad_idxs = []
             for vert in quad:
-                quad_idxs.append(np.where((vertices == vert).all(axis=1))[0][0])
-            processed_quads[i, :] = quad_idxs
+                quad_idxs.append(np.where((self.tracker.vertices == vert).all(axis=1))[0][0])
+            self.processed_quads[i, :] = quad_idxs
 
-        assert(processed_quads.shape == (64*32, 4))
-        processed_quads = np.vstack([processed_quads for i in range(num_t)])
-        assert(processed_quads.shape == (64*32*num_t, 4))
-        for i in range(0, processed_quads.shape[0], 64*32):
-            processed_quads[i:(64*32)*(int(i/2048) + 1), :] += len(vertices)*(int(i/2048))
+        assert(self.processed_quads.shape == (64*32, 4))
+        self.processed_quads = np.vstack([self.processed_quads for i in range(num_t)])
+        assert(self.processed_quads.shape == (64*32*num_t, 4))
+        for i in range(0, self.processed_quads.shape[0], 64*32):
+            self.processed_quads[i:(64*32)*(int(i/2048) + 1), :] += len(self.tracker.vertices)*(int(i/2048))
 
-        processed_quads = np.repeat(processed_quads, 2, axis=0)
-        assert(processed_quads.shape == (64*32*2*num_t, 4))
-        processed_quads *= 2
-        processed_quads[np.arange(1, processed_quads.shape[0] + 1, 2)] += 1
-        processed_quads = processed_quads.astype(np.int64)
+        self.processed_quads = np.repeat(self.processed_quads, 2, axis=0)
+        assert(self.processed_quads.shape == (64*32*2*num_t, 4))
+        self.processed_quads *= 2
+        self.processed_quads[np.arange(1, self.processed_quads.shape[0] + 1, 2)] += 1
+        self.processed_quads = self.processed_quads.astype(np.int64)
 
 
-        vertices = vertices.reshape(-1, 1)
-        assert(vertices.shape == (65*33*2, 1))
-        vertices = np.vstack([vertices for i in range(num_t)])
-        assert(vertices.shape == (65*33*2*num_t, 1))
-        weights_and_vert_idxs = weights_and_verts.reshape(-1, 2, 4)
-        assert(weights_and_vert_idxs.shape == (num_s*num_t, 2, 4))
-        weights_and_vert_idxs = np.repeat(weights_and_vert_idxs, 2, axis=0)
-        assert(weights_and_vert_idxs.shape == (num_s*num_t*2, 2, 4))
-        weights_and_vert_idxs[:, 1, :] *= 2
-        x_coords = np.arange(1, weights_and_vert_idxs.shape[0] + 1, 2).astype(np.int64)
-        weights_and_vert_idxs[x_coords, 1, :] += 1
-        non_solved_verts = []
-        vers = np.arange(0, vertices.shape[0], 1)
-        reshaped_idxs = np.array(sorted(list(set(list(np.squeeze(weights_and_vert_idxs[:, 1, :].reshape(-1, 1)))))))
+        self.vertices = self.tracker.vertices.reshape(-1, 1)
+        assert(self.vertices.shape == (65*33*2, 1))
+        self.vertices = np.vstack([self.vertices for i in range(num_t)])
+        assert(self.vertices.shape == (65*33*2*num_t, 1))
+        self.weights_and_verts = self.tracker.weights_and_verts.reshape(-1, 2, 4)
+        assert(self.weights_and_verts.shape == (num_s*num_t, 2, 4))
+        self.weights_and_verts = np.repeat(self.weights_and_verts, 2, axis=0)
+        assert(self.weights_and_verts.shape == (num_s*num_t*2, 2, 4))
+        self.weights_and_verts[:, 1, :] *= 2
+        x_coords = np.arange(1, self.weights_and_verts.shape[0] + 1, 2).astype(np.int64)
+        self.weights_and_verts[x_coords, 1, :] += 1
+        self.non_solved_verts = []
+        vers = np.arange(0, self.tracker.vertices.shape[0], 1)
+        reshaped_idxs = np.array(sorted(list(set(list(np.squeeze(self.weights_and_verts[:, 1, :].reshape(-1, 1)))))))
         for v in vers:
             if v not in reshaped_idxs:
-                non_solved_verts.append(v)
-        
-        # print("Non Solved Verts Length: ", len(non_solved_verts))
-        # print("Solved Verts Length: ", reshaped_idxs.shape[0])
-        # print("Total Length: ", len(non_solved_verts) + reshaped_idxs.shape[0])
-        # print("Lenght of all vertices", vertices.shape[0])
-        # print("Ea", 65*33*2*num_t)
-    
-        return K_a, vertices, weights_and_vert_idxs, non_solved_verts, processed_quads
+                self.non_solved_verts.append(v)
 
-    def energy_function_lsq(self, num_s, num_t, ka, vertices, weights_and_verts, non_solved_verts, quads, l=None):
-        #2*num_s*num_t -> E_a: each s has 4 x coord weights and 4 y coord weights (each set of weights has a row)..total of t frames
-        #2*num_s*8*num_t -> E_s: each s has 4 points associated with it (quad), each point has 2 coordinates (x coord, y coord), each coord is part of 2 equations
-        ea_rows = ka.shape[0] + len(non_solved_verts)
+    def draw_rectangles(self, v_prime):
+        print("drawing")
+        f = self.tracker.reference_frame.copy()
+        count = 0
+        h, w = self.tracker.reference_frame.shape[0], self.tracker.reference_frame.shape[1]
+        for i in range(0, self.processed_quads.shape[0], 2):
+            y_idxs = np.arange(0, self.vertices.shape[0], 2)
+            x_idxs = np.arange(1, self.vertices.shape[0], 2)
+
+            y_tl = v_prime[int(self.processed_quads[i][0])]
+            x_tl = v_prime[int(self.processed_quads[i + 1][0])]
+
+            y_br = v_prime[int(self.processed_quads[i][3])]
+            x_br = v_prime[int(self.processed_quads[i + 1][3])]
+            if y_tl > 0 and y_tl < h and y_br > 0 and y_br < h:
+                if x_tl > 0 and x_tl < w and x_br > 0 and x_br < w:
+                    count += 1
+                    y_tl = int(np.rint(y_tl))
+                    x_tl = int(np.rint(y_tl))
+                    y_br = int(np.rint(y_br))
+                    x_br = int(np.rint(x_br))
+
+                    cv2.rectangle(f, (x_tl, y_tl), (x_br, y_br), (255, 0, 0), 1)
+
+                    y_tl_o = self.vertices[int(self.processed_quads[i][0])]
+                    x_tl_o = self.vertices[int(self.processed_quads[i + 1][0])]
+
+                    y_br_o = self.vertices[int(self.processed_quads[i][3])]
+                    x_br_o = self.vertices[int(self.processed_quads[i + 1][3])]
+
+                    cv2.rectangle(f, (x_tl_o, y_tl_o), (x_br_o, y_br_o), (0, 0, 255), 1)
+                    cv2.imshow('f', f)
+                    cv2.waitKey(0)
+            if i % (64*32*2) == 0 and i != 0:
+                f = self.tracker.reference_frame.copy()
+                count = 0
+
+    def energy_function_lsq(self, l=None):
+        num_s = self.tracker.feature_table.shape[0]
+        num_t = self.tracker.feature_table.shape[1]
+        ea_rows = self.ka.shape[0] + len(self.non_solved_verts)
         es_rows = 64*32*8*2*num_t
-
         ka_final = np.zeros((ea_rows+es_rows, 1))
-        ka_final[:ka.shape[0]] = ka
-
-        #new matrix that will be A in Ax=b
-        A = np.zeros((ea_rows+es_rows, len(vertices)))
+        ka_final[:self.ka.shape[0]] = self.ka
+        A = np.zeros((ea_rows+es_rows, len(self.vertices)))
 
         #Create K_a equations
-        for i in range(0, weights_and_verts.shape[0]):
-            weights = weights_and_verts[i, 0]
-            vert_idxs = weights_and_verts[i, 1].astype(np.int64)
+        for i in range(0, self.weights_and_verts.shape[0]):
+            weights = self.weights_and_verts[i, 0]
+            vert_idxs = self.weights_and_verts[i, 1].astype(np.int64)
             A[i, vert_idxs] = weights
         
         #Create remaining equations
-        for idx, vert in enumerate(non_solved_verts):
-            offset = weights_and_verts.shape[0]
+        for idx, vert in enumerate(self.non_solved_verts):
+            offset = self.weights_and_verts.shape[0]
             A[idx + offset, vert] = 1
-            ka_final[idx + offset] = vert
+            ka_final[idx + offset] = self.vertices[vert]
 
+        #Create K_s equations
         ea_offset = ea_rows
         for i in range(0, es_rows, 16):
             quad_index = int(i/16)
-            y1 = quads[quad_index, 0]
-            x1 = quads[quad_index + 1, 0]
+            y1 = self.processed_quads[quad_index, 0]
+            x1 = self.processed_quads[quad_index + 1, 0]
 
-            y2 = quads[quad_index, 1]
-            x2 = quads[quad_index + 1, 1]
+            y2 = self.processed_quads[quad_index, 1]
+            x2 = self.processed_quads[quad_index + 1, 1]
             
-            y3 = quads[quad_index, 2]
-            x3 = quads[quad_index + 1, 2]
+            y3 = self.processed_quads[quad_index, 2]
+            x3 = self.processed_quads[quad_index + 1, 2]
 
-            y4 = quads[quad_index, 3]
-            x4 = quads[quad_index + 1, 3]
+            y4 = self.processed_quads[quad_index, 3]
+            x4 = self.processed_quads[quad_index + 1, 3]
             
             v1 = np.array([y1, x1])
             v2 = np.array([y2, x2])
@@ -121,13 +150,7 @@ class LeastSquaresSolver:
             combos_x = np.array([[x1, x2, x4], [x4, x2, x1], [x1, x3, x4], [x4, x3, x1], [x2, x1, x3], [x3, x1, x2], [x2, x4, x3], [x3, x4, x2]]).astype(np.int64)
             combos_y = np.array([[y1, y2, y4], [y4, y2, y1], [y1, y3, y4], [y4, y3, y1], [y2, y1, y3], [y3, y1, y2], [y2, y4, y3], [y3, y4, y2]]).astype(np.int64)
             combos = [[v1, v2, v4], [v4, v2, v1], [v1, v3, v4], [v4, v3, v1], [v2, v1, v3], [v3, v1, v2], [v2, v4, v3], [v3, v4, v2]]
-            # top left: v1
-            # top right: v2
-            # bottom left: v3
-            # bottom right: v4
-            # [x1 y1] = [x2 y2] + [y4 - y2, x2 - x4]
-            #x: index0x - index1x - index2y + index1y  
-            #y: index0y - index1y - index1x + index2x
+
             for j in range(0, len(combos)*2, 2):
                 x_idxs = np.array([combos_x[j//2, 0], combos_x[j//2, 1], combos_y[j//2, 2], combos_y[j//2, 1]])
                 y_idxs = np.array([combos_y[j//2, 0], combos_y[j//2, 1], combos_x[j//2, 1], combos_x[j//2, 2]])
@@ -138,13 +161,25 @@ class LeastSquaresSolver:
                 A[i + j + ea_offset, y_idxs] = np.array(y_coeffs)
                 A[i + j + 1 + ea_offset, x_idxs] = np.array(x_coeffs)
 
+
         print("Solving")
         new_w = scipy.sparse.lil_matrix(A, dtype='double')
 
-        v_prime = scipy.sparse.linalg.lsqr(new_w.tocsr(), ka_final); # solve w/ csr
+        v_prime = scipy.sparse.linalg.lsqr(new_w.tocsr(), ka_final) # solve w/ csr
         v_prime = v_prime[0]
+        max_y = 0
+        max_x = 0
+        # for i in range(0, len(v_prime), 2):
+        #     if v_prime[i] > 1280 or v_prime[i] < 0:
+        #         print(self.vertices[i], v_prime[i])
+        #     if v_prime[i + 1] > 720 or v_prime[i + 1] < 0:
+        #         print(self.vertices[i + 1], v_prime[i + 1])
+            # max_y = max(v_prime[i], max_y)
+            # max_x = max(v_prime[i + 1], max_x)
 
-
+        # print(max_y, max_x)
+        return v_prime
+        
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -157,10 +192,9 @@ def main():
 
     tracker = Tracker(config)
     print("called run")
-
-    feature_table, vertices, quads, quad_dict, quad_indices, weights_and_verts = tracker.run()
+    tracker.run()
     print("called lsq")
-    lsq_solver = LeastSquaresSolver(feature_table, vertices, weights_and_verts, quads)
+    lsq_solver = LeastSquaresSolver(tracker)
 
 if __name__ == '__main__':
     main()